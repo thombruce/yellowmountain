1227570c16abbbb2303f63ee4401b03a6747b766
Thom Bruce <thom@thombruce.com>
2015-06-30 00:54:44 +0100

    restrict access to admin namespace

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,9 @@
 class Admin::AdminController < ApplicationController
   before_action :authenticate_user!
+  before_filter :authorize_user
+
+  private
+  def authorize_user
+    redirect_to root_path unless current_user.has_any_role? :admin, :client
+  end
 end
