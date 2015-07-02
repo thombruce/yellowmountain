e52fcb3435e44a0733e6af0a7ca7c3320ee92528
Thom Bruce <thom@thombruce.com>
2015-07-02 01:11:14 +0100

    try adding email to parameters configuration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # You can put the params you want to permit in the empty array.
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << [:username]
+    devise_parameter_sanitizer.for(:sign_up) << [:email, :username]
   end
 
   # You can put the params you want to permit in the empty array.
   def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << [:username]
+    devise_parameter_sanitizer.for(:account_update) << [:email, :username]
   end
 
   # The path used after sign up.
