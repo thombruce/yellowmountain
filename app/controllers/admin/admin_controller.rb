8944f44e196fb778d5b0dbf8ef2b5ab6bf8f2f19
Thom Bruce <thom@thombruce.com>
2015-07-04 16:22:40 +0100

    admin styles

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,6 +2,8 @@ class Admin::AdminController < ApplicationController
   before_action :authenticate_user!
   before_filter :authorize_user
 
+  layout 'admin'
+
   private
   def authorize_user
     redirect_to root_path unless current_user.has_any_role? :admin, :client
