c4d4c62f5f83930c56769631d76e5442e9a10d71
Thom Bruce <thom@thombruce.com>
2015-07-09 21:08:53 +0100

    add confirmable to devise users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :username, use: [:slugged, :finders]
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
