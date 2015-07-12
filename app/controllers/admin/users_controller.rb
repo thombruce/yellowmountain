871adcbdf605b2ed226731538b1a0b00976fe183
Thom Bruce <thom@thombruce.com>
2015-07-13 00:22:53 +0100

    handling for temporary usernames and vanity_url generation - required for editing or deleting users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,9 +12,10 @@ class Admin::UsersController < Admin::AdminController
   def create
     @user = User.new(user_params)
     if @user.username.blank?
-      @user.username = ('%06d' % rand(10 ** 6)).to_s
+      @user.username = "u_" + Array.new(8){ [*'0'..'9',*'A'..'Z',*'a'..'z'].sample }.join
     end
     if !@user.email.blank?
+      @user.slug = @user.username
       @user.invite!(current_user)
       redirect_to admin_users_path
     else
