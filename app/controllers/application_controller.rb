2b4cd12e885bf2cef4d27a6f1002d9f52e3dd335
Thom Bruce <thom@thombruce.com>
2015-07-14 18:46:16 +0100

    basic internationalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_locale
+    I18n.locale = extract_locale
+  end
+
+  private
+
+  def extract_locale
+    case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}(?:\-[a-zA-Z]{2})?/).first
+      when 'en-GB'
+        'en-GB'
+      when 'en-US'
+        'en'
+      when 'en'
+        'en'
+      else
+        'en'
+    end
+  end
 end
