d3ae713685d9ec6081293b436cba1c4ee06f9b6a
Thom Bruce <thom@thombruce.com>
2015-07-14 03:09:36 +0100

    needed to pluralize organizations

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   has_many :pages, :dependent => :destroy
 
   has_many :organizations, :dependent => :destroy
-  #accepts_nested_attributes_for :organization, :update_only => true
+  accepts_nested_attributes_for :organizations, :update_only => true
 
   has_many :domains, :as => :owner, :dependent => :destroy
 
